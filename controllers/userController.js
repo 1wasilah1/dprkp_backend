04805b0086ff6acbfdeafe7288c32f339cc5cc43
1wasilah1 <m.wasilahhadi@gmail.com>
2025-04-14 08:55:16 +0700

    perubahan case sensitif untuk running di centos

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/UserModel');
-const Menu = require('../models/MenuModel'); 
+// const Menu = require('../models/MenuModel'); 
 
 // Create User
 exports.createUser = async (req, res) => {
